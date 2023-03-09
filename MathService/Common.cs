c35037da81fe3736c6402aa7ecae495d1bf3f2e2
Zulfuqar Asgerzade <37780533+ZulfuqarAsgerzade@users.noreply.github.com>
2023-03-10 00:04:28 +0400

    Add files via upload

---
+++ MathService/Common.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathService
+{
+	public class Common
+	{
+		public bool IsNegative(int number)
+		{
+			return false;
+		}
+
+		public bool IsEven(int number)
+		{
+			return number % 2 == 0;
+		}
+	}
+}
